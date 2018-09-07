f94ec4f307b5fcc6bf592150153e0d105dbc9117
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-09-07 03:08:33 -0300

    Criação da secretKey com hash randômico - md5 Hash Generator

--- NoFood.Api/bin/configuration/variables.js
+++ NoFood.Api/bin/configuration/variables.js
@@ -4,6 +4,9 @@ const variables = {
     },
     Database:{
         connection: process.env.connection || 'mongodb://admin:nofood123456@ds245762.mlab.com:45762/nofood'
+    },
+    Security:{
+        secretKey: 'd41d8cd98f00b204e9800998ecf8427e|733f463f2caee158e4d56a07364d527f'
     }
 }
 
